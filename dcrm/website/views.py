8c131d13c09ce66d232ac9620e8ef38f7d65ebd7
Chubber279 <lorenzo.ocampo279@gmail.com>
2023-09-24 09:42:02 +0800

    Display records

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 def home(request):
+    records  = Record.objects.all()
+    
+
     # Check to see if logging in
     if request.method == "POST":
         username = request.POST.get('username')
@@ -20,7 +24,7 @@ def home(request):
             messages.error(request, "There was an Error Loggin In, Please Try Again...")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {"records": records})
 
 def login_user(request):
     pass
